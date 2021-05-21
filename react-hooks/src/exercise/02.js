15022be0a211cddf4efe26d7d2b347e915f8820e
ssonko94 <ssonkojimmy@gmail.com>
2021-05-21 17:09:11 +0300

    create a custom hook that exports the functionality of local storage into a reusable function

--- react-hooks/src/exercise/02.js
+++ react-hooks/src/exercise/02.js
@@ -3,20 +3,26 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem('name') || defaultValue,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(
-    () => window.localStorage.getItem('name') || initialName,
-  )
+  const [name, setName] = useLocalStorageState('name', initialName)
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
-
-  React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
